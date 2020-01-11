d75a6b8d9d5e78d1945ef34c074b7c3f129657d9
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-01-10 21:01:13 -0300

    #

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,40 +1,44 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import './../css/bootstrap.min.css'
 function Nav() {
 
   return (
-
-    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-      
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <NavLink className="navbar-brand" to="/">Logo</NavLink>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-     
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/about-us">Sobre Nós</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/contact">Contato</NavLink>
-        </li>
-
-        
-        <li className="nav-item dropdown">
-          <NavLink className="nav-link dropdown-toggle" to="/" id="navbardrop" data-toggle="dropdown">
-            Dropdown link
-          </NavLink>
-          <div className="dropdown-menu">
-            <NavLink className="dropdown-item" to="/category?id=1">Categoria 1</NavLink>
-            <NavLink className="dropdown-item" to="/category?id=2">Categoria 2</NavLink>
-            <NavLink className="dropdown-item" to="/category?id=3">Categoria 3</NavLink>
-          </div>
-        </li>
-      </ul>
-    </nav> 
-
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about-us">Sobre Nós</NavLink>
+          </li>
+          <li className="nav-item dropdown">
+            <NavLink className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </NavLink>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <NavLink class="dropdown-item" to="/">Action</NavLink>
+              <NavLink class="dropdown-item" to="/">Another action</NavLink>
+              <NavLink class="dropdown-item" to="#">Something else here</NavLink>
+            </div>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/contact">Contato</NavLink>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="text" placeholder="Email" aria-label="Email" />
+          <input className="form-control mr-sm-2" type="password" placeholder="Senha" aria-label="Senha" />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+        </form>
+      </div>
+    </nav>
   )
 
  }
